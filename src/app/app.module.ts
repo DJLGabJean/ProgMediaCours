e4b8543716f43bc59d2b7fabafe39660f3d54a0e
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-09-19 11:00:40 +0200

    Arriver jusqu'à l'étape Property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { FilmComponent } from './film/film.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     FilmComponent
   ],
   imports: [
--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-film',
   templateUrl: './film.component.html',
   styleUrls: ['./film.component.css']
 })
-export class FilmComponent {
+export class FilmComponent implements OnInit {
+
+  filmName: string = 'Jurrasic Park';
+  filmOnAir: string = 'En Blue Ray';
+  filmAffiche: string = "https://upload.wikimedia.org/wikipedia/fr/6/6a/Jurassic_Park_logo.svg"
+
+  constructor() { }
+
+  ngOnInit()  {
+    return this.filmName;
+  }
+
+  getOnAir() {
+    return this.filmOnAir;
+  }
 
 }
