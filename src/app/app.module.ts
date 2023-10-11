349eb6afe6cd151378824d729dc7f3da20c4b682
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-10-11 16:41:57 +0200

    Commencement du cours Angular sur les routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { FilmComponent } from './film/film.component';
 import { FormsModule } from '@angular/forms';
 
 import { FilmService } from './services/film.service';
+import { HomeComponent } from './home/home.component';
+import { FilmListComponent } from './film-list/film-list.component';
+import { FilmModifComponent } from './film-modif/film-modif.component';
+import { FilmNewComponent } from './film-new/film-new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilmComponent
+    FilmComponent,
+    HomeComponent,
+    FilmListComponent,
+    FilmModifComponent,
+    FilmNewComponent
   ],
   imports: [
     BrowserModule,
